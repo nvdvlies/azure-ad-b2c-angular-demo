82d6600fbb902591831c605bd8f6ec4b23054172
Niels van der Vlies <nvlies@hotmail.com>
2019-03-05 20:14:43 +0100

    MsalModule import, registered MsalInterceptor

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { MsalModule, MsalInterceptor } from '@azure/msal-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +15,26 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule.forRoot({
+      clientID: '875f4002-f526-422c-ad8e-b3ddbce25841',
+      authority: 'https://login.microsoftonline.com/tfp/AzureB2CDemo3.onmicrosoft.com/B2C_1_signin/',
+      redirectUri: 'http://localhost:4200',
+      consentScopes: [
+        '875f4002-f526-422c-ad8e-b3ddbce25841'
+      ],
+      protectedResourceMap : [
+        ['https://localhost:44340/api/', ['875f4002-f526-422c-ad8e-b3ddbce25841']]
+      ]
+    })
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
