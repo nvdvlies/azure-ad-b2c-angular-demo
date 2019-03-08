0dd04dc3864f9725549a434bb74021911e788ca1
Niels van der Vlies <nvlies@hotmail.com>
2019-03-08 15:03:45 +0100

    Added endpoint that returns claim values

---
+++ web-api/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var claims = User.Claims.Select(claim => $"{claim.Type} = {claim.Value}").ToList();
+
+            return Ok(claims);
+        }
+    }
+}
--- web-api/Startup.cs
+++ web-api/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
